c1d7cd5cbd57123c0464a4032797a1a4624c8f4a
MedBel <moebelhassen123@gmail.com>
2023-02-26 21:21:25 +0100

    adding files

---
+++ waa.js
@@ -0,0 +1,17 @@
+const set1 = [3, 1, 7, 9];
+const set2 = [2, 4, 1, 9, 3];
+const distinct = [];
+for (let i = 0; i < set1.length; i++) {
+  if (!distinct.includes(set1[i])) {
+    distinct.push(set1[i]);
+  }
+}
+for (let i = 0; i < set2.length; i++) {
+  if (!distinct.includes(set2[i])) {
+    distinct.push(set2[i]);
+  }
+}
+let sum = 0;
+for (let i = 0; i < distinct.length; i++) {
+  sum += distinct[i];
+}
